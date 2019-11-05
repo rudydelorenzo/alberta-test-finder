be922a6dcf5f83d1ddfed8c9cd5d666431b5a595
rudydelorenzo <rdelorenzo5@gmail.com>
2019-11-05 05:00:27 -0700

    Added mail basic functionality

--- src/testfinder/TestFinder.java
+++ src/testfinder/TestFinder.java
@@ -1,6 +1,15 @@
 package testfinder;
 
 import java.util.*;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.Select;
@@ -8,9 +17,17 @@ import org.openqa.selenium.support.ui.Select;
 
 public class TestFinder {
     
-    static String baseURL = "https://scheduler.itialb4dmv.com/SchAlberta/Applicant/Information";
-    static ArrayList<Appointment> previousResult = new ArrayList();
-    static int interval = 10; //in seconds
+    public static String baseURL = "https://scheduler.itialb4dmv.com/SchAlberta/Applicant/Information";
+    public static ArrayList<Appointment> previousResults = new ArrayList();
+    public static int interval = 10; //in seconds
+    
+    //email stuff
+    public static String to = "rdelorenzo5@gmail.com";
+    public static String from = "rdelorenzo5@gmail.com";
+    public static final String username = "rdelorenzo5@gmail.com"; //change accordingly
+    public static final String password = "Dancemusic5"; //change accordingly
+    public static String host = "smtp.gmail.com";
+    public static int port = 465;
     
     public static void main(String[] args) {
         try {
@@ -74,27 +91,60 @@ public class TestFinder {
                         newAppointment.setAddress(card.findElement(By.className("card-body")).findElement(By.tagName("p")).getText());
                         newAppointment.setDate(card.findElement(By.cssSelector("[id^=dateVal]")).getText());
                         newAppointment.setTime(card.findElement(By.cssSelector("[id^=timeVal]")).getText());
-                        //newAppointment.printInfo();
                         appointments.add(newAppointment);
                     }
                     //compare previous result to current result
                     boolean different = false;
-                    if (appointments.size() == previousResult.size()) {
+                    if (appointments.size() == previousResults.size()) {
                         for (int i = 0; i<appointments.size(); i++) {
-                            if (!appointments.get(i).equals(previousResult.get(i))) different = true;
+                            if (!appointments.get(i).equals(previousResults.get(i))) different = true;
                         }
                     } else {
                         different = true;
                     }
                     if (different) {
                         //compose and send mail
+                        Properties props = new Properties();
+                        props.put("mail.smtp.host", host);
+                        props.put("mail.smtp.port", port);
+                        props.put("mail.smtp.auth", "true");
+                        props.put("mail.smtp.socketFactory.port", port);
+                        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                        
+                        Session session = Session.getInstance(props,
+                                new javax.mail.Authenticator() {
+                                    protected PasswordAuthentication getPasswordAuthentication() {
+                                        return new PasswordAuthentication(username, password);
+                                    }
+                                });
+                        
+                        try {
+
+                            Message message = new MimeMessage(session);
+                            message.setFrom(new InternetAddress(from));
+                            message.setRecipients(
+                                    Message.RecipientType.TO,
+                                    InternetAddress.parse(to)
+                            );
+                            message.setSubject("New Road Test Openings!");
+                            message.setText("Dear Mail Crawler,"
+                                    + "\n\n Please do not spam my email!");
+
+                            Transport.send(message);
+                        } catch (MessagingException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("EMail sent");
                     }
                 } else {
                     //there are no openings
                     System.out.println("Veredict: No Openings");
                 }
                 //transfer contents from current results to previous results
-                //TODO
+                previousResults = new ArrayList();
+                for (Appointment a : appointments) {
+                    previousResults.add(a);
+                }
                 Thread.sleep(interval*1000);  // Pause before trying again
                 content.findElement(By.cssSelector("button[onclick='goBack()']")).click();
                 //flip two lines above for slightly better performance
