cc866ea234b31a160aed9c58b566f26aa6b32a0a
rudydelorenzo <rdelorenzo5@gmail.com>
2019-11-22 02:49:40 -0700

    Created new main class Now requires flags to launch

---
+++ src/main/main.java
@@ -0,0 +1,40 @@
+package main;
+
+public class main {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String noArgs[] = {""};
+            switch(args[0]) {
+                case "-h":
+                    printHelp();
+                    break;
+                case "-help":
+                    printHelp() ;
+                    break;
+                case "-t":
+                    testfinder.instanceLauncher.main(noArgs);
+                    break;
+                case "-s":
+                    ATFServer.atfServerMain.main(noArgs);
+                    break;
+                default:
+                    printHelp();
+                    break;
+            }
+        } else {
+            printHelp();
+        }
+    }
+    
+    public static void printHelp() {
+        String helpText = 
+                "Welcome to the Alberta Test Finder launcher!\n"
+                + "Here are the supported flags:\n"
+                + "\t\"-t\"\tlaunches the test finder application.\n"
+                + "\t\"-s\"\tlaunches the ATF server to accept new subscribers.\n"
+                + "\t\"-h\"\tshows accepted flags\n"
+                + "\t\"-help\"";
+        System.out.println(helpText);
+    }
+    
+}
